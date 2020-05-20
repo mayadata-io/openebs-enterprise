b6f9a8100661582a7584ba1878385bad05dba54e
kmova <kiran.mova@mayadata.io>
2020-05-20 18:32:44 +0000

    fix(ee-tagging): update to 1.10 containers
    Signed-off-by: kmova <kiran.mova@mayadata.io>

--- openebs-ee/docker-images/tag-ee.sh
+++ openebs-ee/docker-images/tag-ee.sh
@@ -8,23 +8,29 @@ ENTERPRISE_REG="mayadataio/"
 
 usage()
 {
-	echo "Usage: $0 <openebs version>"
-	exit 1
+  echo "Usage: $0 <community version> [<rc-tag>]"
+  echo "Example: To tag 1.10.0-ee from 1.10.0, use: $0 1.10.0"
+  echo "Example: to tag 1.10.0-ee-RC3 from 1.10.0, use: $0 1.10.0 RC3"
+  exit 1
 }
 
-if [ $# -ne 1 ]; then
-	usage
+if [ $# -lt 1 ]; then
+  usage
 fi
 
-RELEASE_TAG=$1
+C_REL=$1
+EE_SUFFIX="-ee"
+if [ ! -z $2 ]; then 
+  EE_SUFFIX="-ee-${2}"
+fi
 
 IMGLIST=$(cat  openebs-community-images.txt |tr "\n" " ")
 
 for IMG in $IMGLIST
 do
-  docker pull ${COMMUNITY_REG}${IMG}:$RELEASE_TAG
-  docker tag ${COMMUNITY_REG}${IMG}:$RELEASE_TAG ${ENTERPRISE_REG}${IMG}:$RELEASE_TAG-ee
-  docker push ${ENTERPRISE_REG}${IMG}:$RELEASE_TAG-ee
+  echo "docker pull ${COMMUNITY_REG}${IMG}:${C_REL}"
+  echo "docker tag ${COMMUNITY_REG}${IMG}:${C_REL} ${ENTERPRISE_REG}${IMG}:${C_REL}${EE_SUFFIX}"
+  echo "docker push ${ENTERPRISE_REG}${IMG}:${C_REL}${EE_SUFFIX}"
 done
 
 #Images that do not follow the openebs release version
@@ -32,7 +38,7 @@ TIMGLIST=$(cat  openebs-community-fixed-tags.txt |tr "\n" " ")
 
 for TIMG in $TIMGLIST
 do
-  docker pull ${COMMUNITY_REG}${TIMG}
-  docker tag ${COMMUNITY_REG}${TIMG} ${ENTERPRISE_REG}${TIMG}-ee
-  docker push ${ENTERPRISE_REG}${TIMG}-ee
+  echo "docker pull ${COMMUNITY_REG}${TIMG}"
+  echo "docker tag ${COMMUNITY_REG}${TIMG} ${ENTERPRISE_REG}${TIMG}${EE_SUFFIX}"
+  echo "docker push ${ENTERPRISE_REG}${TIMG}"
 done
